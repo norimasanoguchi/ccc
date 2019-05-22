bb60ebf12fe265fbc617af5220ecd256e3b4de94
norimasanoguchi <norimasanoguchi@gmail.com>
2019-05-22 20:48:21 +0900

    Fix:Userモデルから未使用関連付け削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, :trackable
-  has_one :visa
-  has_one :prefecture
 
   validates :name, presence: true
   validates :birthday, presence: true
