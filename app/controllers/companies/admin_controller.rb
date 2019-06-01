3af962ada795179879755a7abf0cac420870e31b
norimasanoguchi <norimasanoguchi@gmail.com>
2019-06-01 17:24:06 +0900

    Add:Ransack使うCompanies:Adminにストロング パラメーター追記

--- app/controllers/companies/admin_controller.rb
+++ app/controllers/companies/admin_controller.rb
@@ -3,15 +3,19 @@ class Companies::AdminController < ApplicationController
 
  def index
    @current_company = current_company
-   # @users = User.all
    @sexes = User.sexes
    @visas = Visa.all
    @edu_levels = User.edu_levels
    @jlpts = User.jlpts
    @Prefectures = Prefecture.all
    @chinese_skills = User.chinese_skills
-   @q = User.ransack(params[:q])
-   @users = @q.result
+   if params[:q].present?
+     @q = User.ransack(search_params)
+     @users = @q.result
+   else
+     @q = User.ransack(params[:q])
+     @users = User.all
+   end
  end
 
   def show
@@ -21,4 +25,13 @@ class Companies::AdminController < ApplicationController
   def user_info
     @user = User.find_by(id:params[:id])
   end
+end
+
+private
+
+def search_params
+  params.require(:q)
+    .permit(:name_cont,:email_cont,:tel_cont,:wechat_id_cont,
+            :birthday_to_age_gteq,:birthday_to_age_lteq,:sex_eq,:prefecture_id_eq,
+            :visa_id_eq,:edu_level_in,:jlpt_in,:chinese_skill_in)
 end
