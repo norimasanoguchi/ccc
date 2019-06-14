6b9e8cd0b3b4c2c58e2a15154572c14e8ae4811b
norimasanoguchi <norimasanoguchi@gmail.com>
2019-06-14 20:07:40 +0900

    Add:Userのmessage画面テスト

---
+++ spec/factories/conversation.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :conversation do
+    user_id { '1' }
+    company_id { '1' }
+  end
+end
--- spec/user/admin.rb
+++ spec/user/admin.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 describe 'ユーザー管理画面', type: :system do
   before do
-    Factory.Bot.create(:user, name:'test', email: 'test@mail.com', email: 'password')
+    let(:user_a){ Factory.Bot.create(:user, name:'test', email: 'test@mail.com', email: 'password') }
     visit new_user_session_path
-    fill_in 'user_email', with: 'test@mail.com'
-    fill_in 'user_password', with: 'password'
+    fill_in 'user_email', with: user_a.email
+    fill_in 'user_password', with: user_a.password
     click_button 'ログイン'
     visit users_admin_index_path
   end
@@ -31,14 +31,24 @@ describe 'ユーザー管理画面', type: :system do
     end
   end
 
-  context 'メッセージを操作する' do
+  context 'メッセージ機能を利用する場合' do
     before do
-      #メッセージを作る
+      let(:company_a){ Factory.Bot.create(:company) }
+      Factory.Bot.create(:conversation, user_id: user_a.id , company_id: company_a.id)
     end
 
-    it 'メッセージを送る' do
-      #メッセージを見る操作
-      # メッセージを送る操作
+    it 'メッセージが正常に送れる' do
+      #ユーザーログイン画面に行く処理？
+      click_button 'メッセージ詳細'
+      fill_in 'Type a message', with: 'test message'
+      expect(page).to have_content 'test message'
+    end
+
+    it 'メッセージが空の時にバリデーションが写る' do
+      #ユーザーログイン画面に行く処理？
+      click_button 'メッセージ詳細'
+      fill_in 'Type a message', with: ''
+      expect(page).to have_content 'バリデーションに失敗しました: Bodyを入力してください'
     end
 
   end
